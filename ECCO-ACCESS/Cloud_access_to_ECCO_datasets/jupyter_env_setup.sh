150fa6dd9a943751fd626a7c267d430e1465033d
Andrew Delman <andrewdelman@ucla.edu>
2024-02-08 21:59:23 +0000

    updates to jupyter_env_setup.sh

--- ECCO-ACCESS/Cloud_access_to_ECCO_datasets/jupyter_env_setup.sh
+++ ECCO-ACCESS/Cloud_access_to_ECCO_datasets/jupyter_env_setup.sh
@@ -12,22 +12,31 @@
 # ~/ECCO-v4-Python-Tutorial/ECCO-ACCESS/Cloud_access_to_ECCO_datasets/jupyter_env_setup.sh
 
 
+
 # # Start body of script
+
+red_start='\033[0;31m'
+blue_start='\033[0;34m'
+nocolor_start='\033[0m'
 
 # install wget
 sudo dnf install wget -y
+echo -e "${red_start}Installed wget${nocolor_start}"
 
-# retrieve and install miniforge in /opt/
+# install tmux
+sudo dnf install tmux -y
+echo -e "${red_start}Installed tmux${nocolor_start}"
+
+# retrieve and install miniforge in /tmp/
 # assuming EBS volume is already attached to instance
+echo -e "${red_start}Starting Miniforge3 installation${nocolor_start}"
 mkdir -p /tmp
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh" -O /tmp/Miniforge3.sh
-sudo bash /tmp/Miniforge3.sh -b -p /opt/conda
-sudo chmod -R a+w /opt/conda
-source "/opt/conda/etc/profile.d/conda.sh"
-source "/opt/conda/etc/profile.d/mamba.sh"
+bash /tmp/Miniforge3.sh -b -p /tmp/conda
+rm -f /tmp/Miniforge.sh
+source "/tmp/conda/etc/profile.d/conda.sh"
+source "/tmp/conda/etc/profile.d/mamba.sh"
 
-red_start='\033[0;31m'
-nocolor_start='\033[0m+'
 echo -e "${red_start}Completed Miniforge3 installation${nocolor_start}"
 
 # add conda and mamba to path
@@ -46,6 +55,7 @@ echo -e "${red_start}Created jupyter environment${nocolor_start}"
 
 # install python packages (using mamba) in jupyter environment
 mamba activate jupyter
+echo -e "${red_start}Installing Python packages in jupyter environment${nocolor_start}"
 mamba install requests tqdm numpy pandas -y
 mamba install xorg-libice libexpat libevent -y
 mamba install nspr alsa-lib libogg libpq -y
@@ -90,12 +100,14 @@ pip install ecco_v4_py
 
 echo -e "${red_start}Completed Python package installations${nocolor_start}"
 
+echo -e "${red_start}Setting up NASA Earthdata authentication${nocolor_start}"
 # NASA Earthdata authentication
 # check if credentials are already archived in ~/.netrc, and if not then prompt the user for them
 earthdata_cred_stored=0
 if [ -f ~/.netrc ]; then
     if grep -q "machine urs.earthdata.nasa.gov" ~/.netrc; then
         earthdata_cred_stored=1
+        echo -e "${red_start}Earthdata credentials already archived"
     fi
 fi
 if [ $earthdata_cred_stored -eq 0 ]; then
@@ -103,21 +115,20 @@ if [ $earthdata_cred_stored -eq 0 ]; then
     read -p 'NASA Earthdata username: ' uservar
     read -sp 'NASA Earthdata password: ' passvar
     echo -e "machine urs.earthdata.nasa.gov\n    login ${uservar}\n    password ${passvar}\n" >> ~/.netrc
+    
+    echo -e "\n${red_start}NASA Earthdata authentication info archived in ~\/.netrc${nocolor_start}"
 fi
 sudo chmod 400 ~/.netrc
 
-echo -e "${red_start}Set up NASA Earthdata authentication${nocolor_start}"
-
 # set up Jupyter lab, to be opened in a tmux session using password
 PW="$(python3 -c 'from notebook.auth import passwd; import getpass; print(passwd(getpass.getpass(), algorithm="sha256"))')"
-jlab_start="\'jupyter lab --no-browser --autoreload --port=9889 --ip=\'127.0.0.1\' --NotebookApp.token=\'\' --NotebookApp.password=\"$PW\" --notebook-dir=\"~/ECCO-v4-Python-Tutorial/Tutorials_as_Jupyter_Notebooks\" \'"
+jlab_start="\'mamba activate jupyter && jupyter lab --no-browser --autoreload --port=9889 --ip=\'127.0.0.1\' --NotebookApp.token=\'\' --NotebookApp.password=\"$PW\" --notebook-dir=\"~/ECCO-v4-Python-Tutorial/Tutorials_as_Jupyter_Notebooks\" \'"
 tmux new-session -d -s jupyterlab ${jlab_start}
 
-blue_start='\033[0;34m'
 echo -e "${red_start}Started Jupyter lab in tmux session jupyterlab"
 echo -e "${red_start}Access from your local machine in a browser window at"
 echo -e "${blue_start}http://127.0.0.1:9889/"
 echo -e "${red_start}tmux session can be accessed with"
 echo -e "${blue_start}tmux -a -t jupyterlab"
 echo -e "${red_start}and terminated with"
-echo -e "${blue_start}tmux kill-ses -t jupyterlab"
+echo -e "${blue_start}tmux kill-ses -t jupyterlab${nocolor_start}"
