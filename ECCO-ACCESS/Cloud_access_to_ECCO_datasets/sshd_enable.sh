8f15489cb64bc5387525d6099425f3887e90d333
Andrew Delman <andrew.s.delman@gmail.com>
2024-02-16 00:50:53 +0000

    fixes to sshd_enable.sh

--- ECCO-ACCESS/Cloud_access_to_ECCO_datasets/sshd_enable.sh
+++ ECCO-ACCESS/Cloud_access_to_ECCO_datasets/sshd_enable.sh
@@ -4,11 +4,22 @@
 # on EC2 instances where they are disabled by default
 # (e.g., with JPL AMIs).
 # 
+# This script must be run as root, otherwise an error is returned:
+# $ sudo ./sshd_enable.sh
+# 
 # Once this script runs successfully,
 # it should be possible to login to the instance using ssh, e.g.:
 #
 # $ ssh -i "~/.ssh/key_pair.pem" jpluser@private_ip_address
 
+
+# Return error if not running as root
+if [ $( whoami ) != "root" ]; then
+    echo "Error: this script must be run as root"
+    echo "Please re-run using sudo, e.g.:"
+    echo "$ sudo ./sshd_enable.sh"
+    exit 1
+fi
 
 
 # Try to enable sshd
@@ -23,7 +34,7 @@ else
          
         # Re-try enabling sshd
         systemctl enable sshd
-        if [ $? -eq 0 ] ; then
+        if [ $? -eq 0 ]; then
             echo "Enabled sshd successfully"
         else
             echo "Error: symlink deletion did not allow sshd to be enabled"
@@ -38,6 +49,7 @@ fi
 if [ ! -f /usr/lib/systemd/system/sshd.service ]; then
     ln -s /etc/systemd/system/multi-user.target.wants/sshd.service /usr/lib/systemd/system/sshd.service
     echo "Created symlink to sshd.service"
+fi
  
 # create new ssh keys
 ssh-keygen -q -N "" -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key
@@ -51,12 +63,12 @@ echo '$ ssh -i "~/.ssh/your_key_pair.pem" jpluser@private_ip_address'
 
 
 # move git repo to ssh user's directory and change ownership (if requested)
-read -p 'Move ECCO-v4-Python-Tutorial repo to different user? [Y/N] ' move_opt
+read -p 'Move ECCO-v4-Python-Tutorial repo to different user? (Y/[N]) ' move_opt
 if [ $move_opt == "Y"] || [ $move_opt == "y" ]; then
     read -p 'User name of new owner [jpluser for JPL]: ' ssh_user
     cd /home
     mv ./ssm-user/ECCO-v4-Python-Tutorial ./${ssh_user}/
     echo "Moved ECCO-v4-Python-Tutorial repo to /home/${ssh_user}/"
-    chown -R ${ssh_user}:${ssh_user} ./{ssh_user}/ECCO-v4-Python-Tutorial
+    chown -R ${ssh_user}:${ssh_user} ./${ssh_user}/ECCO-v4-Python-Tutorial
     echo "Changed owner and group of git repo to ${ssh_user}"
 fi
